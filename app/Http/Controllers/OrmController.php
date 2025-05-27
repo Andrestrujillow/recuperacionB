4250b84267089dcf2621eb79483008c76c841f4f
Brandon Trujillo <brandontrujillo2bact@gmail.com>
2025-05-26 19:14:26 -0500

    brandonandres

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -14,26 +14,32 @@ class OrmController extends Controller
     {
         // 1. Obtener los libros escritos por un autor (author -> books)
         //$authors = author::find(1);
-        //return $authors->books;
+        //return $authors->book;
+        //return $authors;
 
         // 2. Obtener el autor de un libro (book -> author)
         //$books = book::find(1);
         //return $books->author;
+        //return $books;
 
         // 3. Obtener las copias de un libro (book -> copies)
         //$books = book::find(1);
-         //return $books->copies;
+         //return $books->copy;
+         //return $books;
 
         // 4. Obtener el libro de una copia (copy -> book)
         // $copies = copy::find(1);
         // return $copies->book;
+        //return $copies;
 
         // 5. Obtener los usuarios que tienen una copia (copy -> users)
         // $copies = copies::find(1);
-        // return $copy->users;
+        // return $copies->user;
+        //return $copies;
 
         // 6. Obtener las copias que tiene un usuario (user -> copies)
-        // $user = user::find(1);
-        // return $user->copies;
+        // $users = user::find(1);
+        // return $users->copy;
+        //return $users;
     }
 }
